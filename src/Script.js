d03a9af2d6dc68b857a70c94a66f8429e569c251
Srijan Shetty <36466814+srijan-shetty@users.noreply.github.com>
2021-10-06 14:19:34 +0530

    Delete Script.js

--- src/Script.js
+++
@@ -1,4 +0,0 @@
-function revelMessage() {
-    location.href = ("file:///C:/Users/bd1714/Desktop/Data/MOC_BACTH_DASHBOARD_2021-08-11.html")
-    /*document.getElementById("hiddenMessage").style.display ='block';*/
-}
